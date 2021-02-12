8b9f6562e4cd642e1432488c06ee9897fb71f31d
IvanGer212 <Comradeivan88@gmail.com>
2021-02-12 11:12:59 +0300

    Add new class Worker and constructor for this class

---
+++ src/GeekBrain_Java_level1/Worker.java
@@ -0,0 +1,24 @@
+package GeekBrain_Java_level1;
+
+public class Worker {
+    private String name;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private float sallary;
+    private int age;
+
+    public Worker(String name, String position, String email, String phoneNumber, float sallary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.sallary = sallary;
+        this.age = age;
+    }
+
+    public Worker() {
+
+    }
+
+}
